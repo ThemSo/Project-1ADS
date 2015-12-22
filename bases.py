20e550d81db567a58308da7ca615039070175ac6
Kerakse <214717@supinfo.com>
2015-12-22 14:33:44 +0100

    Create bases.py
    3.1 initialisation mini projet

---
+++ bases.py
@@ -0,0 +1,13 @@
+def bases(n):
+    plateau = [[0]*n for i in range(n)]
+    for ligne in plateau:
+        for x in ligne:
+            print(x, ' ', end='')
+    print('\n')
+    return plateau
+bases(6)
+
+def list_deux_dimension (l):
+    l=[[]]
+    print(l)
+list_deux_dimension(list)
