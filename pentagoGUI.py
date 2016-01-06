cb589359a9938f1b83cac7189acd38811f93525f
Kaeldazs <camille.sardin@gmail.com>
2016-01-06 18:45:11 +0100

    0.6

--- pentagoGUI.py
+++ pentagoGUI.py
@@ -33,7 +33,6 @@ def draw_plateau(plateau):
                 addy = -gutter//4
             else:
                 addy = gutter//4
-            print(addy, addx)
             draw_x = ((ix*height_square)+padding+addx)+round((height_square-30)/2)
             draw_y = ((iy*height_square)+padding+addy)+round((height_square-30)/2)
 
@@ -45,28 +44,61 @@ RED = (152, 0, 0)
 DARK_RED = (131, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-gutter = 4
-padding = 60
-plateau = bases(6)
-plateau = [
-	[0, 1, 1, 1, 0, 1],
-	[1, 0, 0, 1, 2, 0],
-	[0, 0, 0, 2, 1, 1],
-	[1, 0, 1, 2, 1, 1],
-	[1, 2, 2, 2, 0, 0],
-	[0, 1, 1, 0, 1, 1],
-]
+gutter = 6
+padding = 20
+columns = 6
+plateau = bases(columns)
+animation = False
 
 pygame.init()
 
 screen_size = (800, 500)
 screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
 img = [pygame.image.load('img/0.png'), pygame.image.load('img/1.png'), pygame.image.load('img/2.png')]
 pygame.display.set_caption('Pentago')
 render()
 
+
+# retourne la position du click tel que :
+# pos = pos_click_plateau(mouse_pos)
+# y = pos[0]
+# x = pos[1]
+# et donc :
+# plateau[y][x]
+# retourne la valeur de la case cliquée
+def pos_click_plateau(pos):
+    height = screen_size[1]
+    height_cadrant = round((height-padding*2)/2)
+    x, y = pos[0]-padding, pos[1]-padding
+    height_square = round(((height-(padding*2))/len(plateau)))
+    if y >= height_cadrant-gutter/2:
+        if height_cadrant+gutter/2 > y:
+            y = -1
+        else:
+            y -= gutter//2
+
+    if x >= height_cadrant-gutter/2:
+        if height_cadrant+gutter/2 > x:
+            x = -1
+        else:
+            x -= gutter//2
+    x //= height_square
+    y //= height_square
+    if columns > x >= 0 and columns > y >= 0:
+        return y, x
+    return -1, -1
+
+
 while running:
     event = pygame.event.wait()
+    if event.type == pygame.MOUSEBUTTONUP:
+        mouse_pos = pygame.mouse.get_pos()
+        print(pos_click_plateau(mouse_pos))
     if event.type == pygame.QUIT:
         running = False
+    elif animation:
+        render()
+        clock.tick(60)
+
 pygame.quit()
