2266fac29f12bc976987d1cc1e67b513ac03794e
Kaeldazs <camille.sardin@gmail.com>
2016-01-09 17:33:41 +0100

    Ajout animation

--- pentagoGUI.py
+++ pentagoGUI.py
@@ -1,4 +1,6 @@
 import pygame
+import math
+import time
 
 
 def bases(n):
@@ -9,55 +11,34 @@ def bases(n):
 def render():
     screen.fill(BLACK)
     draw_plateau(plateau)
-    pygame.display.flip()
+    pygame.display.update()
 
 
 def draw_plateau(plateau):
     height = screen_size[1]
     height_cadrant = round((height-padding*2)/2)
-    mid_gutter = round(gutter/2)
-    pygame.draw.rect(screen, RED, (padding, padding, height_cadrant-mid_gutter, height_cadrant-mid_gutter), 0)
-    pygame.draw.rect(screen, RED, (padding+height_cadrant+mid_gutter, padding, height_cadrant-mid_gutter, height_cadrant-mid_gutter), 0)
-    pygame.draw.rect(screen, RED, (padding+height_cadrant+mid_gutter, padding+height_cadrant+mid_gutter, height_cadrant-mid_gutter, height_cadrant-mid_gutter), 0)
-    pygame.draw.rect(screen, RED, (padding, padding+height_cadrant+mid_gutter, height_cadrant-mid_gutter, height_cadrant-mid_gutter), 0)
-    height_square = round(((height-(padding*2))/len(plateau)))
+    mid_GUTTER = round(GUTTER/2)
+    pygame.draw.rect(screen, RED, (padding, padding, height_cadrant-mid_GUTTER, height_cadrant-mid_GUTTER), 0)
+    pygame.draw.rect(screen, RED, (padding+height_cadrant+mid_GUTTER, padding, height_cadrant-mid_GUTTER, height_cadrant-mid_GUTTER), 0)
+    pygame.draw.rect(screen, RED, (padding+height_cadrant+mid_GUTTER, padding+height_cadrant+mid_GUTTER, height_cadrant-mid_GUTTER, height_cadrant-mid_GUTTER), 0)
+    pygame.draw.rect(screen, RED, (padding, padding+height_cadrant+mid_GUTTER, height_cadrant-mid_GUTTER, height_cadrant-mid_GUTTER), 0)
+    height_square = ((height-(padding*2))/len(plateau))
     for iy, y in enumerate(plateau):
         for ix, x in enumerate(y):
 
             if ix < (len(y)+1)//2:
-                addx = -gutter//2
+                addx = -GUTTER//4
             else:
-                addx = 0
+                addx = GUTTER//4
 
             if iy < (len(y)+1)//2:
-                addy = -gutter//2
+                addy = -GUTTER//4
             else:
-                addy = gutter//4
-            draw_x = ((ix*height_square)+padding+addx)+round((height_square-30)/2)
-            draw_y = ((iy*height_square)+padding+addy)+round((height_square-30)/2)
+                addy = GUTTER//4
+            draw_x = ((ix*height_square)+padding+addx)+(height_square-30)/2
+            draw_y = ((iy*height_square)+padding+addy)+(height_square-30)/2
 
             screen.blit(img[x], (draw_x, draw_y))
-
-
-running = True
-RED = (152, 0, 0)
-DARK_RED = (131, 0, 0)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-gutter = 6
-padding = 20
-columns = 6
-plateau = bases(columns)
-animation = False
-
-pygame.init()
-
-screen_size = (800, 500)
-screen = pygame.display.set_mode(screen_size)
-clock = pygame.time.Clock()
-img = [pygame.image.load('img/0.png'), pygame.image.load('img/1.png'), pygame.image.load('img/2.png')]
-pygame.display.set_caption('Pentago')
-render()
 
 
 # retourne la position du click tel que :
@@ -71,41 +52,86 @@ def pos_click_plateau(pos):
     height = screen_size[1]
     height_cadrant = round((height-padding*2)/2)
     x, y = pos[0]-padding, pos[1]-padding
-    height_square = round(((height-(padding*2)-gutter)/len(plateau)))
-    if y >= height_cadrant-gutter/2:
-        if height_cadrant+gutter/2 > y:
+    height_square = round(((height-(padding*2)-GUTTER)/len(plateau)))
+    if y >= height_cadrant-GUTTER/2:
+        if height_cadrant+GUTTER/2 > y:
             y = -1
         else:
-            y -= gutter//2
+            y -= GUTTER//2
 
-    if x >= height_cadrant-gutter/2:
-        if height_cadrant+gutter/2 > x:
+    if x >= height_cadrant-GUTTER/2:
+        if height_cadrant+GUTTER/2 > x:
             x = -1
         else:
-            x -= gutter//2
+            x -= GUTTER//2
     x //= height_square
     y //= height_square
     if columns > x >= 0 and columns > y >= 0:
-        return x, y
+        return round(x), round(y)
     return -1, -1
 
-def player_pion(mouse_pos, player):
+
+def resize_plateau(start, end_value, duration):
+    global padding
+    start_value = padding
+    current_time = time.time()*1000
+    while current_time-start <= duration:
+        current_time = time.time()*1000
+        padding = round(ease(current_time-start, start_value, end_value, duration))
+        render()
+        clock.tick(FPS)
+
+
+# t: temps actuel, b: valeur de départ, c: valeur finale, d: durée
+def ease(t, b, c, d):
+    t = t/d-1
+    return -c * (t*t*t*t - 1) + b
+
+
+def pose_pion(mouse_pos, player):
     pos = pos_click_plateau(mouse_pos)
     x = pos[0]
     y = pos[1]
     if plateau[y][x] == 0 and x != -1:
+        global STEP
         plateau[y][x] = player
-    render()
+        render()
+        resize_plateau(time.time()*1000, padding_step_2, 600)
+        STEP = 2
 
+
+running = True
+RED = (152, 0, 0)
+DARK_RED = (131, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+FPS = 60
+GUTTER = 6
+PLAYER = 1
+STEP = 1
+padding_step_2 = 60
+if STEP == 2:
+    padding = padding_step_2
+else:
+    padding = 20
+
+columns = 6
+plateau = bases(columns)
+
+pygame.init()
+
+screen_size = (800, 500)
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+img = [pygame.image.load('img/0.png'), pygame.image.load('img/1.png'), pygame.image.load('img/2.png')]
+pygame.display.set_caption('Pentago')
+render()
 
 while running:
     event = pygame.event.wait()
     if event.type == pygame.MOUSEBUTTONUP:
-        print(pos_click_plateau(pygame.mouse.get_pos()))
+        if STEP == 1:
+            pose_pion(pygame.mouse.get_pos(), PLAYER)
     if event.type == pygame.QUIT:
         running = False
-    elif animation:
-        render()
-        clock.tick(60)
-
 pygame.quit()
